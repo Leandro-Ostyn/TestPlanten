d56c7a4c32160dcb188437fd42137223d599de67
Leandro <R0797275@student.vives.be>
2020-02-19 09:21:50 +0100

    Aanmaken GUI

--- src/PlantenGui/Controller.java
+++ src/PlantenGui/Controller.java
@@ -1,4 +1,19 @@
 package PlantenGui;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public Button btnVooruit;
+    public Button btnAchteruit;
+    public TextField txtInvoerPlant;
+    public TextArea txtUitvoerPlant;
+
+    public void clicked_Vooruit(MouseEvent mouseEvent) {
+    }
+
+    public void clicked_Achteruit(MouseEvent mouseEvent) {
+    }
 }
